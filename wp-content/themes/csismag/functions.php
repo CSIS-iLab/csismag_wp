080c7aeebf437b9a1db60d7c9691667c271ac859
Jacque Schrag <jnschrag@gmail.com>
2020-05-13 16:26:36 -0400

    Rename CSIS Journalism block plugin

--- wp-content/themes/csismag/functions.php
+++ wp-content/themes/csismag/functions.php
@@ -117,6 +117,11 @@ function csismag_theme_support() {
 	// Add support for full and wide align images.
 	add_theme_support( 'align-wide' );
 
+	/* Disable custom font sizes, colors, gradients in Blocks */
+	add_theme_support( 'disable-custom-font-sizes' );
+	add_theme_support( 'disable-custom-colors' );
+	add_theme_support( 'disable-custom-gradients' );
+
 	/*
 	 * Adds starter content to highlight the theme on fresh sites.
 	 * This is done conditionally to avoid loading the starter content on every
