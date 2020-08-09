d9653f74e5d5768fe33cc272ee87370e9dca49f9
Jacque Schrag <jnschrag@gmail.com>
2020-08-09 13:20:55 -0400

    Commit color & split templates

---
+++ wp-content/themes/csismag/templates/split.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Split Text Template
+ * Template Post Type: post
+ *
+ * @package CSIS iLab
+ * @subpackage @package CSISMag
+ * @since 1.0
+ */
+
+get_template_part( 'singular' );
