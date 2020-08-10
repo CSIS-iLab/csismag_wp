36d75c55e89b516fbd70117d80665cde80c75ccf
Jacque Schrag <jnschrag@gmail.com>
2020-08-10 10:46:04 -0400

    Add color & split templates again

---
+++ wp-content/themes/csismag/templates/minimal.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Minimal Template
+ * Template Post Type: post
+ *
+ * @package CSIS iLab
+ * @subpackage @package CSISMag
+ * @since 1.0
+ */
+
+get_template_part( 'singular' );
